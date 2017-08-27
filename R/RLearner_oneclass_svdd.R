cc11cd0910886cb42d960b4ad86326dfcc6f236e
pfistfl <pfistererf@googlemail.com>
2017-08-27 17:08:15 +0200

    Fix svdd pars

--- R/RLearner_oneclass_svdd.R
+++ R/RLearner_oneclass_svdd.R
@@ -6,14 +6,15 @@ makeRLearner.oneclass.svdd = function() {
     par.set = makeParamSet(
       makeDiscreteLearnerParam(id = "kernel", default = "rbfdot",
         values = c("vanilladot", "polydot", "rbfdot", "tanhdot", "laplacedot")),
-      makeNumericLearnerParam(id = "c.reg", lower = 0, default = 1),
+      makeNumericLearnerParam(id = "c.reg", lower = 0, upper = 1, default = NULL, special.vals = list(NULL)),
+      makeNumericLearnerParam(id = "anomaly.rate", lower = 0, upper = 1, default = NULL, special.vals = list(NULL)),
       makeNumericLearnerParam(id = "sigma",
-        lower = 0, requires = quote(kernel %in% c("rbfdot", "laplacedot"))),
+        lower = 0, requires = quote(kernel %in% c("rbfdot", "laplacedot")), default = NULL, upper = Inf),
       makeIntegerLearnerParam(id = "degree", default = 3L, lower = 1L,
         requires = quote(kernel %in% c("polydot"))),
-      makeNumericLearnerParam(id = "scale", default = 1, lower = 0,
+      makeNumericLearnerParam(id = "scale", default = 1, lower = 0, upper = Inf,
         requires = quote(kernel %in% c("polydot", "tanhdot"))),
-      makeNumericLearnerParam(id = "offset", default = 1,
+      makeNumericLearnerParam(id = "offset", default = 1, upper = Inf, lower = -Inf
         requires = quote(kernel %in% c("polydot", "tanhdot")))
     ),
     properties =  c("oneclass", "numerics"),
